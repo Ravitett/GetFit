8dbe15a1e37ce42decdfbc9704cfedfce428afe4
Ravitett <ravitett@gmail.com>
2022-02-09 14:50:36 +0200

    v

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-require("./server/index");
+require("./db");
+
+const express = require('express');
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+const jwt = require("jsonwebtoken");
+const {checkToken} = require("./auth/checkToken");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const {authRouter} = require('./auth/authRouter');
+app.use('/auth', authRouter);
+
+const {userR} = require('./routers/user');
+app.use('/api/users', checkToken, userR);
+
+const {activityR} = require('./routers/activity');
+app.use('/api/activities',checkToken, activityR);
+
+const {workoutR} = require('./routers/workout');
+app.use('/api/workouts',checkToken, workoutR);
+
+
+
+app.all('*',(req,res) => {
+    res.send("Page not found");
+});
+
+
+app.listen(process.env.PORT || 3005);
